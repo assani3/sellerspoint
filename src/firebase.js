6af98935a6ffe72aa8191cf35ab3f5a030baca0e
assani3 <assanishabani51@gmail.com>
2025-05-22 11:50:25 +0200

    Favicon set up

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDSv9tPnnl9Dk9B-vxPliq3A22BCBchOjc",
-  authDomain: "sellerspointt.firebaseapp.com",
-  projectId: "sellerspointt",
-  storageBucket: "sellerspointt.firebasestorage.app",
-  messagingSenderId: "321795876195",
-  appId: "1:321795876195:web:20e04499d8ac3c46229f56",
-  measurementId: "G-0CL3BM01ZH"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+
+const firestore = firebase.firestore();
+export { firestore };
